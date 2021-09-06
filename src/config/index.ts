dc3c273683f0d24abcd35dd80592fdf3cab46bb2
clarkche <clarkchenme@gmail.com>
2021-09-06 10:39:27 +0800

    feat: 回溯功能修改

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,7 @@
 import TokenConfig from "./Tokens";
+const ETH_RPC = process.env.ETH_RPC||"http://39.105.105.233:3335" //office run time defuatl
 
 const configs = {
-    TokenConfig
+    TokenConfig, ETH_RPC
 }
 export default configs
